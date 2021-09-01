38e9fab26ba76fbcb0f44d632a26c7b864405d49
jldoucette <jd@jonathandoucette.com>
2021-09-01 12:11:33 -0700

    Update email2sms.js
    Fixed Paren

--- api/email2sms.js
+++ api/email2sms.js
@@ -22,7 +22,7 @@ module.exports = async (req, res) => {
     console.log("*****FROM NAME***** "+fromName);
     console.log("*****FROM***** "+from);
     console.log("*****TO***** "+to);
-    if (toName != "+14158238255" && (fromName != "jldoucette.work" || fromName != "mw.alerts")
+    if (toName != "+14158238255" && (fromName != "jldoucette.work" || fromName != "mw.alerts"))
         console.log("#######NOT VALIDATED-FAIL########")
         return;
    console.log("^^^^^PASSED VALIDATION^^^^^^^");
