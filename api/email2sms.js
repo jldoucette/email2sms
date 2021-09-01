a32493db0015a7fd4b0094274750284a1a3ac407
jldoucette <jd@jonathandoucette.com>
2021-09-01 15:45:32 -0700

    Update email2sms.js

--- api/email2sms.js
+++ api/email2sms.js
@@ -22,10 +22,10 @@ module.exports = async (req, res) => {
     console.log("*****FROM NAME***** "+fromName);
     console.log("*****FROM***** "+from);
     console.log("*****TO***** "+to);
-    if (toName != "14158238255" && fromName != "mw.alerts")
+        if (fromName != "mw.alerts")
         console.log("#######NOT VALIDATED-FAIL########")
         return;
-   console.log("^^^^^PASSED VALIDATION^^^^^^^");
+   console.log("###PASSED VALIDATION###");
     //Sending SMS with Twilio Client
     client.messages.create({
         to: `+${toName}`,
