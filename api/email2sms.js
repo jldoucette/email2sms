4d0d68bd622eccb6859b8a72025e584632729b13
jldoucette <jd@jonathandoucette.com>
2021-09-01 11:52:54 -0700

    Update email2sms.js
    Added condition to block all but correct source/destination

--- api/email2sms.js
+++ api/email2sms.js
@@ -18,13 +18,13 @@ module.exports = async (req, res) => {
     const toName = toAddress.local;
     const fromAddress = addrs.parseOneAddress(from);
     const fromName = fromAddress.local;
-    console.log("*****TO ADDRESS***** "+toAddress);
     console.log("*****TO NAME***** "+toName);
-    console.log("*****FROM ADDRESS***** "+fromAddress);
     console.log("*****FROM NAME***** "+fromName);
     console.log("*****FROM***** "+from);
     console.log("*****TO***** "+to);
-    
+    if (toName <> "14158238255" && fromName<>"mw.alerts")
+        return;
+   
     //Sending SMS with Twilio Client
     client.messages.create({
         to: `+${toName}`,
